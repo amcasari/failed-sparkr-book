c36a25a3a9339b10815f8dc9018e46cdafdb5445
morningc <morningc@users.noreply.github.com>
2015-09-30 21:44:34 -0700

    cleaning up for final review

--- ch4/R/convert-local-dataframe.R
+++ ch4/R/convert-local-dataframe.R
@@ -2,8 +2,11 @@ library(SparkR)
 sc <- sparkR.init(master="local[2]",appName="convert-local-dataframe")
 sqlContext <- sparkRSQL.init(sc)
 
-# we'll test out converting Michelson Speed of Light Data from R's datasets package
-df <- createDataFrame(sqlContext, morley)
+# we'll test out converting morley, Michelson Speed of Light Data from R's datasets package
+r_df <- morley
+str(r_df)
+
+df <- createDataFrame(sqlContext, r_df)
 head(df)
 
 sparkR.stop()
