77f1fb77ac06431bd05e7452e06b936bc661b290
morningc <morningc@users.noreply.github.com>
2015-10-09 21:01:02 -0700

    working on explore

--- ch5/R/explore.R
+++ ch5/R/explore.R
@@ -1,6 +1,26 @@
 # import dataframe
 
+sc <- sparkR.init(master="local[2]",appName="explore")
+sqlContext <- sparkRSQL.init(sc)
+
+# we'll test out converting morley, Michelson Speed of Light Data from R's datasets package
+r_df <- morley
+str(r_df)
+
+df <- createDataFrame(sqlContext, r_df)
+
+# find out more about df
+head(df) # returns a data.frame
+dtypes(df) # returns a list
+columns(df) # returns a character
+printSchema(df) # no return
+
+describe(df)
+summary(df)
+
 # column access
+head(select(df, "Expt"))
+df
 
 # create new columns
 
@@ -17,3 +37,5 @@
 # initial viz: histograms
 
 # initial viz: scatter plots
+
+sparkR.stop()
