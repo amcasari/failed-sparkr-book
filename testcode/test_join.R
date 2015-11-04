aaa769f6b0d90285df3c95d78d9595f314f6e4be
Deborah Siegel <deborah.siegel@gmail.com>
2015-11-04 11:28:44 -0800

    Update test_join.R

--- testcode/test_join.R
+++ testcode/test_join.R
@@ -2,22 +2,20 @@
 #adapted directly from spark project's  test_sparkSQL.R
 #for the purpose of testing simple examples of code and learning
 
-n = c(2, 3, 5)
-s = c("aa", "bb", "cc")
-b = c(TRUE, FALSE, TRUE)
-df = data.frame(n, s, b)
-df1= createDataFrame(sqlContext, df)
-showDF(df1)
-
-x = c(2, 3, 10)
-t = c("dd", "bb", "ff")
-c = c(FALSE, FALSE, TRUE)
-dff = data.frame(x, t, c)
-df2 = createDataFrame(sqlContext, dff)
-showDF(df2)
+n <- c(2, 3, 5)
+s <- c("aa", "bb", "cc")
+b <- c(TRUE, FALSE, TRUE)
+df <- data.frame(n, s, b)
+df1 <- createDataFrame(sqlContext, df)
 
 
-res = join(df1, df2, df1$n == df2$x)
+x <- c(2, 3, 10)
+t <- c("dd", "bb", "ff")
+c <- c(FALSE, FALSE, TRUE)
+dff <- data.frame(x, t, c)
+df2 <- createDataFrame(sqlContext, dff)
+
+res <- join(df1, df2, df1$n == df2$x)
 showDF(res)
 
 df3 <- join(df1, df2, df1$n==df2$x & df1$s==df2$t)
