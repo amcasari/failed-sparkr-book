15bd2fc7db024bb09435b722a2762f0b312051ab
Deborah Siegel <deborah.siegel@gmail.com>
2015-11-04 11:25:03 -0800

    Create test_join.R

---
+++ testcode/test_join.R
@@ -0,0 +1,22 @@
+#command line code
+#adapted directly from spark project's  test_sparkSQL.R
+#for the purpose of testing simple examples of code and learning
+
+n = c(2, 3, 5)
+s = c("aa", "bb", "cc")
+b = c(TRUE, FALSE, TRUE)
+df = data.frame(n, s, b)
+df1= createDataFrame(sqlContext, df)
+showDF(df1)
+
+x = c(2, 3, 10)
+t = c("dd", "ee", "ff")
+c = c(FALSE, FALSE, TRUE)
+dff = data.frame(x, t, c)
+df2 = createDataFrame(sqlContext, dff)
+showDF(df2)
+
+
+res = join(df1, df2, df1$n == df2$x)
+showDF(res)
+
